80e8893d991426a284dfa57770ce0d505e4bc70b
Sakshinayak-2003 <nayaksakshi949@gmail.com>
2024-09-12 20:18:08 +0530

    Add files via upload

---
+++ prasunet task1/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('nav_bar');
+    navbar.style.transition = '3s';
+    navbar.style.backgroundColor = 'white';
+});
